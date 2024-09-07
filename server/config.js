4e8a1e637d92706258541c8bbd4d0110bfae4a99
Shubhamnagar21 <shubhamnagar5000@gmail.com>
2024-09-08 02:22:48 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,3 @@
+const username = encodeURIComponent("sachinnagar");
+const password = encodeURIComponent("Nagar@123");
+export const mongodbURL = `mongodb+srv://${username}:${password}@cluster0.fj16rrv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
